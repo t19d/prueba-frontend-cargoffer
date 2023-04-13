7303aa5587b5aed31db6c9e02e209282d9e25784
david_tojo <david_tojo@hotmail.es>
2023-04-13 22:07:58 +0200

    feat: add authorization to product service HTTP requests using JWT token from auth service

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,28 +11,38 @@ export class ProductService {
 
   private baseUrl = 'http://127.0.0.1:3000/products';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
-  getAllProducts(): Observable<Product[]> {
+  getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
   }
 
-  getProductById(id: number): Observable<Product> {
+  getProductById(id: string | null): Observable<Product> | undefined {
+    if (!id) return;
     const url = `${this.baseUrl}/${id}`;
     return this.http.get<Product>(url);
   }
 
-  createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
+  createProduct(product: Product): Observable<Product> | undefined {
+    if (!product) return;
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post<Product>(this.baseUrl, product, { headers });
   }
 
-  updateProduct(id: number, product: Product): Observable<Product> {
+  updateProduct(id: string, product: Product): Observable<Product> | undefined {
+    if (!id || !product) return;
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     const url = `${this.baseUrl}/${id}`;
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product, { headers });
   }
 
-  deleteProduct(id: number): Observable<Product> {
+  deleteProduct(id: string): Observable<Product> | undefined {
+    if (!id) return;
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url, { headers });
   }
 }
