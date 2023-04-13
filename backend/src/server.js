cd70d49d4e88d33e12fa4713339f23bda5ffb7a7
david_tojo <david_tojo@hotmail.es>
2023-04-13 21:44:45 +0200

    Add CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,20 @@ require('./database/mongodb');
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
 
 // #region Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+// Configurar CORS
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Authorization', 'Content-Type']
+    })
+);
 
 // #endregion
 
