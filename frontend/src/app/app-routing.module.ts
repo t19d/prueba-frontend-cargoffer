637ea940b8a54cbc8c6f1548bfaf936bde98f19a
david_tojo <david_tojo@hotmail.es>
2023-04-13 21:50:19 +0200

    feat: add product details route to AppRoutingModule

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
   { path: 'product-form', component: ProductFormComponent, canActivate: [AuthGuard] },
+  { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'login', component: LoginComponent }
 ];
 
