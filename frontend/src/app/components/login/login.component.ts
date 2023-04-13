d8f15cf276470f80b440e898cf251d6b24cdcc12
david_tojo <david_tojo@hotmail.es>
2023-04-13 14:49:34 +0200

    Add login component with form and AuthService integration

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    this.authService.login(this.username, this.password)
+      .subscribe(result => {
+        if (result === true) {
+          this.router.navigate(['/product-form']);
+        } else {
+          this.errorMessage = 'Username or password is incorrect';
+        }
+      });
+  }
+}
