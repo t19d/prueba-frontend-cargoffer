a0f17cc034410a5a48f437bff5a4ef23647afe0d
david_tojo <david_tojo@hotmail.es>
2023-04-13 21:58:09 +0200

    feat: Implement ability to edit and save product details

--- frontend/src/app/components/product-details/product-details.component.ts
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+  editMode: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {
+    this.getProduct();
+  }
+
+  ngOnInit(): void {
+  }
+
+  getProduct(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.getProductById(id)?.subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  enableEditMode(): void {
+    this.editMode = true;
+  }
+
+  disableEditMode(): void {
+    this.editMode = false;
+  }
+
+  saveChanges(product: Product): void {
+    this.productService.updateProduct(product._id || '', product)?.subscribe((product: Product) => {
+      this.product = product;
+      this.disableEditMode();
+    });
+  }
 
 }
