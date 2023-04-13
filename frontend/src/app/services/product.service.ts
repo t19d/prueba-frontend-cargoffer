9018cbd1dd1b86e46bedd695ca4b7b154d80020b
david_tojo <david_tojo@hotmail.es>
2023-04-13 15:13:30 +0200

    Add ProductService with CRUD operations and API URL

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://127.0.0.1:3000/products';
+
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
+  }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
+  }
+
+  updateProduct(id: number, product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.put<Product>(url, product);
+  }
+
+  deleteProduct(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
+  }
 }
