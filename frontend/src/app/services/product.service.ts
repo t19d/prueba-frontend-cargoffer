1f11faf6fed6d6f15f8b36ae690e14b8c03fa99b
david_tojo <david_tojo@hotmail.es>
2023-04-14 16:30:43 +0200

    feat: implement OWASP security measures

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 import { AuthService } from './auth.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class ProductService {
 
   private baseUrl = 'http://127.0.0.1:3000/products';
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient, private authService: AuthService, private sanitizer: DomSanitizer) { }
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
@@ -19,7 +20,7 @@ export class ProductService {
 
   getProductById(id: string | null): Observable<Product> | undefined {
     if (!id) return;
-    const url = `${this.baseUrl}/${id}`;
+    const url = `${this.baseUrl}/${this.sanitizer.sanitize(4, id.trim())}`;
     return this.http.get<Product>(url);
   }
 
@@ -27,27 +28,42 @@ export class ProductService {
     if (!product) return;
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.post<Product>(this.baseUrl, product, { headers });
+    const body = this.sanitizeProduct(product);
+    return this.http.post<Product>(this.baseUrl, body, { headers });
   }
 
   updateProduct(id: string, product: Product): Observable<Product> | undefined {
     if (!id || !product) return;
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    const url = `${this.baseUrl}/${id}`;
-    return this.http.put<Product>(url, product, { headers });
+    const url = `${this.baseUrl}/${this.sanitizer.sanitize(4, id.trim())}`;
+    const body = this.sanitizeProduct(product);
+    return this.http.put<Product>(url, body, { headers });
   }
 
   deleteProduct(id: string): Observable<Product> | undefined {
     if (!id) return;
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    const url = `${this.baseUrl}/${id}`;
+    const url = `${this.baseUrl}/${this.sanitizer.sanitize(4, id.trim())}`;
     return this.http.delete<Product>(url, { headers });
   }
 
   searchProducts(searchTerm: string): Observable<Product[]> {
-    const url = `${this.baseUrl}?name=${searchTerm}`;
+    const url = `${this.baseUrl}?name=${this.sanitizer.sanitize(4, searchTerm.trim())}`;
     return this.http.get<Product[]>(url);
+  }
+
+  private sanitizeProduct(product: Product): Product {
+    const body: Product = Object.assign({}, product);
+    body.name = this.sanitizer.sanitize(4, product.name.trim()) ?? '';
+    body.description = this.sanitizer.sanitize(4, product.description.trim()) ?? '';
+    body.image = this.sanitizer.sanitize(4, product.image!.trim()) ?? undefined;
+    body.category = this.sanitizer.sanitize(4, product.category!.trim()) ?? undefined;
+    body.size = this.sanitizer.sanitize(4, product.size!.trim()) ?? undefined;
+    body.color = this.sanitizer.sanitize(4, product.color!.trim()) ?? undefined;
+    body.material = this.sanitizer.sanitize(4, product.material!.trim()) ?? undefined;
+    body.brand = this.sanitizer.sanitize(4, product.brand!.trim()) ?? undefined;
+    return body;
   }
 }
