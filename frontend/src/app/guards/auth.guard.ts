e4a0456e246a245224ea0afcceabddb2ae92bc40
david_tojo <david_tojo@hotmail.es>
2023-04-14 17:16:17 +0200

    feat: Add input sanitization

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -3,17 +3,22 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Rout
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { map } from 'rxjs/operators';
+import { sanitizeInput } from '../utils/utils';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private sanitizer: DomSanitizer) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const sanitizedNextRoute = sanitizeInput(this.sanitizer, next.routeConfig?.path ?? '');
+    if (!sanitizedNextRoute) return false;
+
     return this.authService.isLoggedIn().pipe(
       map((loggedIn: boolean) => {
         if (loggedIn) {
