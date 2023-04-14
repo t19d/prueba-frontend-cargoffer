f6dd57800cae7ffd0191999c657be27ec150f28c
david_tojo <david_tojo@hotmail.es>
2023-04-14 09:13:34 +0200

    feat: Add header and footer to app component and implement logout button based on login state

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  isLoggedIn$ = this.authService.isLoggedIn();
+
+  constructor(private authService: AuthService) { }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
