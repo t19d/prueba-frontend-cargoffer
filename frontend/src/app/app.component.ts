5f8d1c7fbca1e792211b2f047e978a618c1b7f45
david_tojo <david_tojo@hotmail.es>
2023-04-14 17:19:46 +0200

    feat: Add navigation to login page on logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,10 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   isLoggedIn$ = this.authService.isLoggedIn();
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   onLogout() {
     this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
